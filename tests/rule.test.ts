af604647be579034a34bc5a669ae21dcb0afddbb
yokotak0527 <454935+yokotak0527@users.noreply.github.com>
2021-11-07 21:10:27 +0900

    ✏️ fix typo

--- tests/rule.test.ts
+++ tests/rule.test.ts
@@ -3,7 +3,7 @@ import Kensho from '../src/Kensho'
 describe(`Add the rule`, ()=>{
   const dummyRule = (value:boolean)=> value
 
-  test(`OK : success`, ()=>{
+  test(`OK : Success`, ()=>{
     expect(Kensho.rule.add('myRule', dummyRule)).toBeUndefined()
     Kensho.rule.remove('myRule')
   })
@@ -16,36 +16,36 @@ describe(`Add the rule`, ()=>{
     Kensho.rule.remove('isTrue')
     Kensho.rule.remove('isFalse')
   })
-  test(`NG : the rule already exist`, ()=>{
+  test(`NG : The rule already exist`, ()=>{
     Kensho.rule.add('myRule', dummyRule)
     expect(()=> Kensho.rule.add('myRule', dummyRule) ).toThrow()
     Kensho.rule.remove('myRule')
   })
-  test(`NG : pass empty string to the 1st argument`, ()=>{
+  test(`NG : Pass empty string to the 1st argument`, ()=>{
     expect(()=> Kensho.rule.add('', dummyRule) ).toThrow()
   })
-  test(`NG : pass other than string to the 1st argument`, ()=>{
+  test(`NG : Pass other than string to the 1st argument`, ()=>{
     expect(()=> Kensho.rule.add(1 as unknown as string, dummyRule) ).toThrow()
   })
-  test(`NG : pass other than function to the 2nd argument`, ()=>{
-    expect(()=> Kensho.rule.add('rule3', 'function') ).toThrow()
+  test(`NG : Pass other than function to the 2nd argument`, ()=>{
+    expect(()=> Kensho.rule.add('rule3', 'function' as unknown as Kensho.DefaultRuleFunction) ).toThrow()
   })
 })
 
 describe(`Remove the rule`, ()=>{
   const dummyRule = (value:boolean)=> value
 
-  test(`OK : success`, ()=>{
+  test(`OK : Success`, ()=>{
     Kensho.rule.add('myRule', dummyRule)
     expect(Kensho.rule.remove('myRule')).toBeUndefined()
   })
-  test(`NG : rule don't existed`, ()=>{
+  test(`NG : The rule don't existed`, ()=>{
     expect(()=> Kensho.rule.remove('myRule') ).toThrow()
   })
-  test(`NG : pass empty string to the argument`, ()=>{
+  test(`NG : Pass empty string to the argument`, ()=>{
     expect(()=> Kensho.rule.remove('') ).toThrow()
   })
-  test(`NG : pass other than string to the argument`, ()=>{
+  test(`NG : Pass other than string to the argument`, ()=>{
     expect(()=> Kensho.rule.remove(1 as unknown as string) ).toThrow()
   })
 })
@@ -53,18 +53,18 @@ describe(`Remove the rule`, ()=>{
 describe('Get the rule', ()=>{
   const dummyRule = (value:boolean)=> value
 
-  test(`OK : success`, ()=>{
+  test(`OK : Success`, ()=>{
     Kensho.rule.add('myRule', dummyRule)
     expect(Kensho.rule.get('myRule')).toBe(dummyRule)
     Kensho.rule.remove('myRule')
   })
-  test(`NG : rule don't existed`, ()=>{
+  test(`NG : The rule don't existed`, ()=>{
     expect(()=> Kensho.rule.get('myRule') ).toThrow()
   })
-  test(`NG : pass empty string to the argument`, ()=>{
+  test(`NG : Pass empty string to the argument`, ()=>{
     expect(()=> Kensho.rule.get('') ).toThrow()
   })
-  test(`NG : pass other than string to the argument`, ()=>{
+  test(`NG : Pass other than string to the argument`, ()=>{
     expect(()=> Kensho.rule.get(1 as unknown as string) ).toThrow()
   })
 })
